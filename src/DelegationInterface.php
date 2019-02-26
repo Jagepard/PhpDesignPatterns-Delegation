9478220fd1e815c2e6e5cd63ca53f65c63de9975
Jagepard <dankorot@gmail.com>
2019-02-26 16:06:28 +0300

    Update

--- src/DelegationInterface.php
+++ src/DelegationInterface.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * @author    : Korotkov Danila <dankorot@gmail.com>
  * @license   https://mit-license.org/ MIT
@@ -15,11 +13,8 @@ namespace Fundamental\Delegation;
  */
 interface DelegationInterface
 {
-
     /**
      * @param DeveloperInterface $developer
      */
     public function delegateTo(DeveloperInterface $developer): void;
-
-    public function getCodeFromDeveloper(): string;
 }
