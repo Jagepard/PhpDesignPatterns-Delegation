c0b69aa2dadd31e5d174af9a37d668eeb789dcae
Jagepard <dankorot@gmail.com>
2019-02-26 16:09:55 +0300

    Add getter

--- tests/DelegationTest.php
+++ tests/DelegationTest.php
@@ -20,11 +20,10 @@ use PHPUnit\Framework\TestCase as PHPUnit_Framework_TestCase;
  */
 class DelegationTest extends PHPUnit_Framework_TestCase
 {
-
     /**
      * @var TeamLead
      */
-    protected $teamLead;
+    private $teamLead;
 
     protected function setUp()
     {
@@ -33,21 +32,29 @@ class DelegationTest extends PHPUnit_Framework_TestCase
 
     public function testTeamLead()
     {
-        $this->assertEquals("Some excellent code", $this->teamLead->writeCode());
+        $this->assertEquals("Some excellent code", $this->getTeamLead()->writeCode());
 
-        $this->teamLead->delegateTo($this->teamLead);
-        $this->assertEquals("Some excellent code", $this->teamLead->getCodeFromDeveloper());
+        $this->getTeamLead()->delegateTo($this->getTeamLead());
+        $this->assertEquals("Some excellent code", $this->getTeamLead()->getCodeFromDeveloper());
     }
 
     public function testMiddle()
     {
-        $this->teamLead->delegateTo(new MiddleDeveloper());
-        $this->assertEquals("Some regular code", $this->teamLead->getCodeFromDeveloper());
+        $this->getTeamLead()->delegateTo(new MiddleDeveloper());
+        $this->assertEquals("Some regular code", $this->getTeamLead()->getCodeFromDeveloper());
     }
 
     public function testJunior()
     {
-        $this->teamLead->delegateTo(new JuniorDeveloper());
-        $this->assertEquals("Some really bad code", $this->teamLead->getCodeFromDeveloper());
+        $this->getTeamLead()->delegateTo(new JuniorDeveloper());
+        $this->assertEquals("Some really bad code", $this->getTeamLead()->getCodeFromDeveloper());
+    }
+
+    /**
+     * @return TeamLead
+     */
+    public function getTeamLead(): TeamLead
+    {
+        return $this->teamLead;
     }
 }
